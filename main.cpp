ebc27d70240d01d01f68122c25babf7f8df50968
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-10-18 17:45:52 -0700

    star matrix

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+        char star='*';
+        unsigned  int i,j;
+        vector <vector <char> >starm;
+        do{
+               cout<< "Enter 2 positive whole numbers."<<endl;
+               cin>>i;
+               cin>>j;
+          } while (i<0 || j<0);
+        starm.resize(i);
+                for(int row=0; row<starm.size();row++)
+        {
+                starm[row].resize(j);
+
+                for(int column=0; column<starm[row].size();column++)
+                {
+                        cout<< star;
+                }
+                cout<<endl;
+
+        }
+
+
+        return 0;
+}
