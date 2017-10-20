7da62ec4ca42234b673d9d0909e100760371201c
KarinaZamacona <azamacona@mail.csuchico.edu>
2017-10-20 15:41:02 -0700

    trying to get rid of extra lines

--- main.cpp
+++ main.cpp
@@ -6,24 +6,24 @@ int main()
         char star='*';
         unsigned  int i,j;
         vector <vector <char> >starm;
-        do{
-               cout<< "Enter rows and columns of stars:"<<endl;
-               cin>>i;
-               cin>>j;
-          } while (i<0 || j<0);
-        starm.resize(i);
-                for(int row=0; row<starm.size();row++)
+        //        do{
+        cout<< "Enter rows and columns of stars:"<<endl;
+        cin>>i;
+        cin>>j;
+        //          } while (i<0 && j<0);
+        if (i>0 && j>0)
         {
-                starm[row].resize(j);
-
-                for(int column=0; column<starm[row].size();column++)
+                starm.resize(i);
+                for(int row=0; row<starm.size();row++)
                 {
-                        cout<< star;
+                        starm[row].resize(j);
+
+                        for(int column=0; column<starm[row].size();column++)
+                        {
+                                cout<< star;
+                        }
+                        cout<<endl;
                 }
-                cout<<endl;
-
         }
-
-
         return 0;
 }
